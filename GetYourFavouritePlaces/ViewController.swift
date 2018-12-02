3af5b6eab06986850ee654d112c5d8ec8ce0ec30
Aleksandra Konopka <aleksandrakonopka@MacBook-Air-Aleksandra.local>
2018-12-02 13:21:53 +0100

    Initial Commit

---
+++ GetYourFavouritePlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetYourFavouritePlaces
+//
+//  Created by Aleksandra Konopka on 02/12/2018.
+//  Copyright © 2018 Aleksandra Konopka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
